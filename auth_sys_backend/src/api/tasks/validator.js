28e7ad60a73b852c73402a8f8a4fcc779115bb65
shivansh983 <121644927+shivansh983@users.noreply.github.com>
2026-06-03 14:43:52 +0530

    Delete auth_sys_backend directory

--- auth_sys_backend/src/api/tasks/validator.js
+++
@@ -1,9 +0,0 @@
-const { z } = require('zod');
-
-const createTaskSchema = z.object({
-  name: z.string().min(3, "Task name must be at least 3 characters"),
-  description: z.string().optional()
-});
-
-
-module.exports = { createTaskSchema };
